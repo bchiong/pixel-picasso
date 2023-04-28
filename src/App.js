6da9fe46a7b86e003911757856d5c71919c007c5
Ben Chiong <benjamin.chiong@lendi.com.au>
2023-04-28 10:43:24 +1000

    add seclect

--- src/App.js
+++ src/App.js
@@ -28,23 +28,6 @@ function App() {
       <div className="flex gap-8">
         <form className="flex-1 space-y-6">
           <div>
-            <legend className="text-left text-lg font-bold mb-2">Number of people:</legend>
-            <div className="flex flex-row flex-wrap gap-4">
-              <div className="flex-auto basis-1/4 flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-                <input id="number-of-people-radio-1" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                <label htmlFor="number-of-people-radio-1" className="w-full py-4 ml-2 text-sm font-medium text-gray-900">1</label>
-              </div>
-              <div className="flex-auto basis-1/4 flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-                <input checked id="number-of-people-radio-2" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                <label htmlFor="number-of-people-radio-2" className="w-full py-4 ml-2 text-sm font-medium text-gray-900">2</label>
-              </div>
-              <div className="flex-auto basis-1/4 flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-                <input checked id="number-of-people-radio-3" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                <label htmlFor="number-of-people-radio-3" className="w-full py-4 ml-2 text-sm font-medium text-gray-900">Several</label>
-              </div>
-            </div>
-          </div>
-          <div>
             <legend className="text-left text-lg font-bold mb-2">Gender:</legend>
             <select id="small" class="block w-full px-4 py-3 text-base mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option selected>Select gender</option>
@@ -52,6 +35,26 @@ function App() {
               <option value="Female">Female</option>
               <option value="Non binary">Neutral</option>
               <option value="DE">They</option>
+            </select>
+          </div>
+          <div>
+            <legend className="text-left text-lg font-bold mb-2">Number of people:</legend>
+            <select id="small" class="block w-full px-4 py-3 text-base mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="Several">Several</option>
+            </select>
+          </div>
+          <div>
+            <legend className="text-left text-lg font-bold mb-2">Age:</legend>
+            <select id="small" class="block w-full px-4 py-3 text-base mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected value="18 year old">18</option>
+              <option value="20 year old">20s</option>
+              <option value="30 year old">30s</option>
+              <option value="30 year old">30s</option>
+              <option value="40 year old">40s</option>
+              <option value="50 year old">50s</option>
+              <option value="60 year old">60s</option>
             </select>
           </div>
         </form>
